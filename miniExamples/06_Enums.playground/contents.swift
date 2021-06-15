4f3c8123f68160afeee7211d0b1287cb71c9b1c9
rhit-yinz1 <yinz1@rose-hulman.edu>
2021-06-15 17:13:56 -0400

    06

--- miniExamples/06_Enums.playground/contents.swift
+++ miniExamples/06_Enums.playground/contents.swift
@@ -1,27 +1,69 @@
 // Basic enum
+enum Weekdays {
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+    
+}
+
+var today: Weekdays
+today  = .thursday
+
+if (today == Weekdays.monday){
+    print("it is Monday")
+}
+
+//today.rawValue
+
+switch today{
+case .monday, .wednesday, .friday:
+    print("Swim Practice")
+case .thursday:
+    print("Horse back riding")
+    
+default:
+    print("Piano practice")
+}
+
+// Rawvalues -----------------------------------------------------------------------------
+
+enum State: Int {
+    case ready;
+    case set;
+    case go;
+}
+
+var raceState = State.ready
+raceState.rawValue
+var nextRaceState = State(rawValue: 0)
+if nextRaceState?.rawValue == 0 {
+    print("ready")
+}
 
 
 
+// Associated values and functions ---------------------------------------------------------------
 
+enum HomeworkStatus {
+    case noHomework
+    case inProgress(Int, Int)
+    case done
+    
+    func simpleDescription() -> String {
+        switch self {
+        case .noHomework:
+            return "No Homework"
+        case .inProgress(let workDone, let totalProblems):
+            return "You have finished \(workDone) out of \(totalProblems)"
+        case .done:
+            return "Done"
+        }
+    }
+}
 
+var myHwStatus = HomeworkStatus.inProgress(8, 10)
 
-
-
-
-// Rawvalues
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Associated values and functions
-
+myHwStatus.simpleDescription()
 
