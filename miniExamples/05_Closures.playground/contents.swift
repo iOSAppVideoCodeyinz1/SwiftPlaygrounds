1e38f3a9fe788ef28c6487ba27505f674913eaca
rhit-yinz1 <yinz1@rose-hulman.edu>
2021-06-15 16:40:47 -0400

    Closures

--- miniExamples/05_Closures.playground/contents.swift
+++ miniExamples/05_Closures.playground/contents.swift
@@ -1,30 +1,55 @@
 var values = [10, 5, 2, 8, 3, 6, 1, 1000]
 
-// Verbose Closure
+// Verbose Closure -----------------------------------------
+
+//func myReducer(runningTotal: Int, currentValue: Int) -> Int {
+//    if currentValue > 5 {
+//        return runningTotal + 1
+//    }else {
+//        return runningTotal
+//    }
+//}
+//
+//var numValuesOver5 = values.reduce(0, myReducer)
+
+//var numValuesOver5 = values.reduce(0, {(runningTotal: Int, currentValue: Int) -> Int in
+//                                    return currentValue > 5 ? runningTotal + 1 : runningTotal})
+
+
+
+
+// Closure parameter name shorthand and trailing closure -----------------------------------------
+
+//var numValuesOver9 = values.reduce(0) {(runningTotal: Int, currentValue: Int) -> Int in
+//                                    return currentValue > 9 ? runningTotal + 1 : runningTotal}
+
+var numValuesOver9 = values.reduce(0) {(runningTotal, currentValue) in
+                                    return currentValue > 9 ? runningTotal + 1 : runningTotal}
+
+//var numValuesOver9 = values.reduce(0) {(runningTotal, currentValue) in
+//                                     currentValue > 9 ? runningTotal + 1 : runningTotal}
+
+//var numValuesOver9 = values.reduce(0) {$1 > 9 ? $0 + 1 : $0}
+//numValuesOver9
+
+// A Closure using a closure for a variable -----------------------------------------
+let numValuesOverLimit = {(myArry: [Int], limit : Int) -> Int in
+    
+    return myArry.reduce(0) {(runningTotal, currentValue) in
+        return currentValue > limit ? runningTotal + 1 : runningTotal}
+
+    
+}
 
 
 
 
 
-// Closure parameter name shorthand and trailing closure
 
 
 
-
-
-
-// A Closure using a closure for a variable
-
-
-
-
-
-
-
-
-
-//numValuesOverLimit(values, limit: 5)
-//numValuesOverLimit(values, limit: 9)
+numValuesOverLimit(values, 5)
+numValuesOverLimit(values, 9)
 
 
 // Comparing func and closure syntax
