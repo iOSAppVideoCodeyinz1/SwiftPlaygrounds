daaddb8c073d3f47c86407080b682194083b0784
rhit-yinz1 <yinz1@rose-hulman.edu>
2021-06-15 22:22:23 -0400

    TicTacToe set

--- ticTacToeGame/TicTacToeGame.playground/contents.swift
+++ ticTacToeGame/TicTacToeGame.playground/contents.swift
@@ -1,11 +1,51 @@
-
-
-// TODO: Create the TicTacToeGame class
-
-
+class TicTacToeGame: CustomStringConvertible {
+    
+    
+    func getBoardString() -> String {
+        var gameString = ""
+        for index in 0..<9 {
+            gameString += board[index].rawValue
+        }
+        return gameString
+    }
+    
+    
+    var description: String{
+        return "\(state) Board: \(getBoardString())"
+    }
+    
+    enum MarkType: String {
+        case none = "-"
+        case x = "X"
+        case o = "O"
+    }
+    
+    var board: [MarkType]
+    
+    enum State: String {
+        case xTurn = "X's Turn"
+        case oTurn = "O's Turn"
+        case xWins = "X Wins"
+        case oWins = "O Wins"
+        case tie = "Tie Game"
+    }
+    
+    var state: State
+    
+    init() {
+        //        board = [MarkType]()
+        //        for _ in 0..<9 {
+        //            board.append(TicTacToeGame.MarkType.none)
+        //        }
+        board = [MarkType](repeating: .none, count: 9)
+        state = .xTurn
+    }
+}
 
 /* ----------------- Official Playground testing ----------------- */
-//var game = TicTacToeGame()
+var game = TicTacToeGame()
+game.board[2] = .x
+game
 //game.pressedSquareAt(0)
 //game.pressedSquareAt(1)
 //game.pressedSquareAt(3)
